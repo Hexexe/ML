aa9235d76f57d4d879846c83060b536d34095148
Hexexe <TusernameAken@gmail.com>
2021-03-02 03:30:01 +0200

    comment fix

--- HighestSummit.py
+++ HighestSummit.py
@@ -1,8 +1,7 @@
 import math
-import random             	# just for generating random mountains                                 	 
+import random                             	 
 
 # generate random mountains                                                                               	 
-
 w = [.05, random.random()/3, random.random()/3]
 h = [1.+math.sin(1+x/.6)*w[0]+math.sin(-.3+x/9.)*w[1]+math.sin(-.2+x/30.)*w[2] for x in range(100)]
 
@@ -11,8 +10,6 @@ def climb(x, h):
     summit = False
     
     #five steps either left or right.
-    #for x_new in range(max(0, x-5), min(99, x+5))
-    # edit here
     while not summit:
         summit = True         # stop unless there's a way up
         for x_new in range(max(0, x-5), min(99, x+5)):
